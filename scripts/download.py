4e897cab77ad03ae5e16ecb5867563be06e720d0
Gaurav Gupta <gouravgupta840@gmail.com>
2024-03-22 12:00:49 +0530

    updated code

--- scripts/download.py
+++ scripts/download.py
@@ -1,15 +1,18 @@
 from common import *
 
-res = supabase.storage.from_(bkt).download(f'{pth}/source.b64')
-if res:
+try:
+  res = supabase.storage.from_(bkt).download(f'{pth}/{b64}')
+  if res:
     clean()
     with open(zip, 'wb+') as z:
-        z.write(base64.b64decode(res))
-        z.close()
-        if os.path.exists(src):
-            shutil.rmtree(src)
-        try:
-            shutil.unpack_archive(zip, src)
-        except Exception as e:
-            print(f"Error extracting '{zip}': {e}")
+      z.write(base64.b64decode(res))
+      z.close()
+      if os.path.exists(src):
+        shutil.rmtree(src)
+      try:
+        shutil.unpack_archive(zip, src)
+      except Exception as e:
+        print(f"Error extracting '{zip}': {e}")
     clean()
+except Exception as e:
+  print(f"Downloading '{pth}/{b64}': {e}")
--- scripts/upload.py
+++ scripts/upload.py
@@ -1,20 +1,20 @@
 from common import *
 
 if not (os.path.exists(src)):
-    print("Source directory not present!")
+  print("Source directory not present!")
 else:
-    clean()
-    zips = zip.split(".")
-    shutil.make_archive(zips[0], zips[1], src)
+  clean()
+  zips = zip.split(".")
+  shutil.make_archive(zips[0], zips[1], src)
 
-    with open(zip, 'rb') as compressed:
-        compress_data = compressed.read()
+  with open(zip, 'rb') as compressed:
+    compress_data = compressed.read()
 
-    ready = base64.b64encode(compress_data).decode('utf-8')
-    with open(b64, 'w') as handle:
-        handle.write(ready)
-        handle.close()
+  ready = base64.b64encode(compress_data).decode('utf-8')
+  with open(b64, 'w') as handle:
+    handle.write(ready)
+    handle.close()
 
-    supabase.storage.from_(bkt).remove(f'{pth}/source.b64')
-    supabase.storage.from_(bkt).upload(file=b64, path=f'{pth}/source.b64', file_options={"content-type": "text/plain", "x-upsert": "true", "cache-control": "3600"})
-    clean()
+  supabase.storage.from_(bkt).remove(f'{pth}/{b64}')
+  supabase.storage.from_(bkt).upload(file=b64, path=f'{pth}/{b64}', file_options={"content-type": "text/plain", "x-upsert": "true", "cache-control": "3600"})
+  clean()
